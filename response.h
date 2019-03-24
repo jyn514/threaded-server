bc791ff4f11ad30772e2445fe92e48960b03e191
Joshua Nelson <jyn514@gmail.com>
2019-03-24 00:23:49 -0400

    add persistent connections
    disable for HTTP/1.0 (e.g. ab)
    fix bug where EOF caused child threads to loop forever

--- response.h
+++ response.h
@@ -6,5 +6,6 @@ struct response {
   char *body; // NOT a string, may not be null terminated
   int length;
   bool is_mmapped;
+  bool persist_connection;
 };
 struct response handle_request(std::string&);
