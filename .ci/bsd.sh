c7d85e1a9f45218618823db22423e62f65e79caf
jyn <github@jyn.dev>
2019-04-11 15:26:24 +0000

    fix permission issues on bsd
    it turns out root can _always_ read files for some reason

--- .ci/bsd.sh
+++ .ci/bsd.sh
@@ -2,4 +2,8 @@
 # running
 pkg install -y gmake
 # testing
-pkg install -y valgrind bats-core apache24 curl
+pkg install -y valgrind bats-core apache24 curl sudo
+# test setup
+mkdir /var/www
+chown www:www /var/www
+chmod a+r test.bats
