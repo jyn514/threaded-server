15cccd6026ee78e9830bd56a1a7bccab7a4bafb0
Joshua Nelson <jyn514@gmail.com>
2019-11-02 12:44:39 -0400

    clean up log

--- src/log.h
+++ src/log.h
@@ -1,12 +1,11 @@
 #include <stdbool.h>
 
-struct log;
+// This is basically a string class under the hood.
 
 struct log *log_init(void);
-// returns false if we ran out of memory
-// len is optional
-// 0 if unused, length of message (including NULL terminator) otherwise
-bool log_write(struct log *, char *format, ...);
+// returns false if an error occured
+// format is the same as for printf
+bool log_write(struct log *, const char *format, ...);
 // returns whether IO succeeded
 bool log_flush(struct log *, FILE *stream);
 void log_free(struct log*);
