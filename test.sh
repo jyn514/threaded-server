eafbd75db1dd7ccb4543c32ff1c238d878fd2701
Joshua Nelson <jyn514@gmail.com>
2019-11-02 10:21:41 -0400

    fix shellcheck complaints

--- test.sh
+++ test.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 [ -z "$BUILD_DIR" ] && export BUILD_DIR=tmp
-export PORT=$(( $RANDOM + 1001 ))
+export PORT=$(( RANDOM + 1001 ))
 
 if [ -n "$1" ]; then MAKE="$1"
 elif [ -x "$(which gmake)" ]; then MAKE=gmake
@@ -15,10 +15,10 @@ fi
 
 $MAKE || exit 1
 
-cd $BUILD_DIR
+cd $BUILD_DIR || exit 2
 $MAIN $PORT >/dev/null &
 sleep 1
-bats $OLDPWD/test.bats
+bats "$OLDPWD/test.bats"
 STATUS=$?
 kill $!
 exit $STATUS
