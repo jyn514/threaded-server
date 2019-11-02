52194d313ab4fdd9cacc34a4c60eb37dad1f5586
Joshua Nelson <jyn514@gmail.com>
2019-11-02 11:38:36 -0400

    remove debug statements

--- src/dict.h
+++ src/dict.h
@@ -4,20 +4,13 @@
 #include <stdio.h>
 #include <stdbool.h>
 
-// Change this to true for debugging output (hitting newline to continue)
-#define DEBUG false
-
-#define debug(s)      if (DEBUG) fprintf(stderr,s)
-#define debug1(s,t)   if (DEBUG) fprintf(stderr,s,t)
-#define debug2(s,t,u) if (DEBUG) fprintf(stderr,s,t,u)
-
 typedef struct dict *DICT;
 typedef void (*mapfunc)(const char *const key,
                         const char *const value, va_list args);
 DICT dict_init(void);
 // returns whether key exists
-// note: these are freed when you call dict_free
-bool dict_put(DICT, char *const key, char *const val);
-char *dict_get(DICT, const char *const key);
+// NOTE: these are freed when you call dict_free (in Rust terms, they are owned value)
+bool dict_put(DICT, char *key, char *val);
+char *dict_get(DICT, const char *key);
 void dict_free(DICT);
 #endif
